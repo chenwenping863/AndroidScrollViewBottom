e57f97d1713661b442ac31eddbd81f4b694b54cd
chenwen <chenwen@mogujie.com>
2017-03-30 18:40:39 +0800

    commit

--- app/src/main/java/com/test/chenwenping/androidscrollbottomtest/MainActivity.java
+++ app/src/main/java/com/test/chenwenping/androidscrollbottomtest/MainActivity.java
@@ -14,11 +14,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mtv = (TextView) findViewById(R.id.text_view);
-        mTextView = (TextView) findViewById(R.id.tx);
-        mTextView.setTextSize(20);
-        mTextView.setText("");
         setContentView(R.layout.activity_main);
+        mtv = (TextView) findViewById(R.id.text_view);
+        mTextView = (TextView) findViewById(R.id.content);
+        mTextView.setText("dsd");
+        mTextView.setTextSize(20);
+      mTextView.setText("Skip to content\n" +
+                "This repository\n" +
+                "Search\n" +
+                "Pull requests\n" +
+                "Issues\n" +
+                "Gist\n" +
+                " @chenwenping863\n" +
+                " Sign out\n" +
+                " Unwatch 1\n" +
+                "  Star 0\n" +
+                " Fork 0 chenwenping863/AndroidScrollViewBottom\n" +
+                " Code  Issues 0  Pull requests 0  Projects 0  Wiki  Pulse  Graphs  Settings\n" +
+                "Quick setup — if you’ve done this kind of thing before\n" +
+                " Set up in Desktop\tor\t\n" +
+                " HTTPS\n");
         mSmartScrollView = (SmartScrollView) findViewById(R.id.smart_scroll_view);
         mSmartScrollView.setScrollViewBottomListener(new SmartScrollView.ScrollViewBottomListener() {
             @Override
